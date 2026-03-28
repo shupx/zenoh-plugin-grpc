2ee17afe3861e0fde53d57f57627a012b87f2dce
Peixuan Shu <shupeixuan@qq.com>
2026-03-28 22:25:37 +0800

    feat: update subscriber declaration to remove allowed_origin parameter

--- zenoh-grpc-client-sdk/zenoh-grpc-python/examples/sub_callback.py
+++ zenoh-grpc-client-sdk/zenoh-grpc-python/examples/sub_callback.py
@@ -4,7 +4,8 @@ import time
 # with zenoh_grpc.Session.connect("unix:///tmp/zenoh-grpc.sock") as session:
 # with zenoh_grpc.Session.connect("tcp://127.0.0.1:7335") as session:
 with zenoh_grpc.Session.connect() as session:
-    sub = session.declare_subscriber("demo/example/**", allowed_origin=1)
+    sub = session.declare_subscriber("demo/example/**")
+    # sub = session.declare_subscriber("demo/example/**", allowed_origin=zenoh_grpc.Locality.ANY)
 
     def on_sample(sample):
         print("\ncallback:", sample.key_expr, sample.payload, sample.encoding)
