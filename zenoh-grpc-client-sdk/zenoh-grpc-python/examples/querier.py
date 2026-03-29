eb30e4a48a0dd7b8f538e188c007d10b082c3e48
Peixuan Shu <shupeixuan@qq.com>
2026-03-29 23:16:07 +0800

    feat: enhance queryable example with blocking receive and multiple replies

--- zenoh-grpc-client-sdk/zenoh-grpc-python/examples/querier.py
+++ zenoh-grpc-client-sdk/zenoh-grpc-python/examples/querier.py
@@ -2,7 +2,11 @@ import zenoh_grpc
 
 
 with zenoh_grpc.Session.connect() as session:
-    querier = session.declare_querier("demo/query/**", consolidation=zenoh_grpc.ConsolidationMode.NONE, timeout_ms=3_000)
+    querier = session.declare_querier(
+        "demo/query/**", 
+        consolidation=zenoh_grpc.ConsolidationMode.NONE, 
+        timeout_ms=3_000)
+    # The querier will receive the replies one by one or all at once depending on the consolidation mode set by the querier (AUTO(default,LATEST), NONE(receive one by one), MONOTONIC(monotonically consolidation), LATEST(only the latest)).
 
     replies = querier.get(payload=b"query from python", encoding="text/plain") # unblocking, returns a ReplyStream
 
--- zenoh-grpc-client-sdk/zenoh-grpc-python/examples/queryable.py
+++ zenoh-grpc-client-sdk/zenoh-grpc-python/examples/queryable.py
@@ -4,11 +4,18 @@ import time
 with zenoh_grpc.Session.connect() as session:
     queryable = session.declare_queryable("demo/query/**", callback=None, complete=False, allowed_origin=zenoh_grpc.Locality.ANY)
 
-    for query in queryable.receiver():
+    while True:
+        query = queryable.receiver().recv() # blocking, returns a Query object when a query is received
         print("\nreceived query:", query.query_id, query.selector, query.key_expr, query.parameters, query.payload, query.encoding, query.attachment)
 
-        query.reply(query.key_expr, b"this is a reply from queryable", encoding="text/plain")
-        print("replied to query:", query.query_id)
+        # You can reply multiple times to the same query by calling reply() multiple times. The querier will receive the replies one by one or all at once depending on the consolidation mode set by the querier (AUTO(default,LATEST), NONE(receive one by one), MONOTONIC(monotonically consolidation), LATEST(only the latest)).
 
+        query.reply(query.key_expr, b"this is a reply1 from queryable", encoding="text/plain")
+        print("reply 1 to query:", query.query_id)
+
+        query.reply(query.key_expr, b"this is a reply2 from queryable", encoding="text/plain")
+        print("reply 2 to query:", query.query_id)
+
+        # Notice: you should explicitly drop() after you finished replying to send ResponseFinal message to querier, otherwise the querier will wait until timeout and will not receive the replies until then.
         query.drop()
         print("dropped query")
