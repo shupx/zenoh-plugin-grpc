8763baf148c9f291d00eb13e9ffeccd3bc27810e
Peixuan Shu <shupeixuan@qq.com>
2026-03-29 23:30:35 +0800

    feat: improve example clarity by adding comments on recv() usage and removing unnecessary code

--- zenoh-grpc-client-sdk/zenoh-grpc-python/examples/sub.py
+++ zenoh-grpc-client-sdk/zenoh-grpc-python/examples/sub.py
@@ -5,6 +5,9 @@ with zenoh_grpc.Session.connect() as session:
     with session.declare_subscriber("demo/example/**") as sub:
         for i in range(1000):
             sample = sub.recv() 
+            # blocking until a sample is received.
             # not recommended to use recv() in a loop, as it is blocking and will block the main thread, and due to the inner rust implementation, it can not be killed by Ctrl+C. It is better to use a callback to receive samples, as shown in sub_callback.py. But here we just want to show how to use recv() in a loop, so we use it here for simplicity.
+
             print(sample.key_expr, sample.payload, sample.encoding)
-            time.sleep(0.1)
+            # print("dropped count:", sub.dropped_count())
+            # time.sleep(1) # do not sleep here, just for testing the dropped count (The low consumption speed leads to the inner subscription queue dropping data.)
